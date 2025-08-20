923c1a9f2753d17a4be22383cca7a5bdbaa6319c
Piyush Mohanty <piyushmohanty509@gmail.com>
2025-08-21 00:57:58 +0530

    add requirements

--- app.py
+++ app.py
@@ -41,21 +41,26 @@ selected_movie_name = st.selectbox(
 if st.button("Recommend"):
     names = recommend(selected_movie_name) 
 
-    col1, col2, col3 ,col4,col5= st.columns(5)
-    with col1:
-        st.text(names[0])
-        # st.image(posters[0])
+    # col1, col2, col3 ,col4,col5= st.columns(5)
+    # with col1:
+    #     st.text(names[0])
+    #     # st.image(posters[0])
 
-    with col2:
-        st.text(names[1])
-        # st.image(posters[1])
+    # with col2:
+    #     st.text(names[1])
+    #     # st.image(posters[1])
 
-    with col3:
-        st.text(names[2])
-        # st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        # st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        # st.image(posters[4])
+    # with col3:
+    #     st.text(names[2])
+    #     # st.image(posters[2])
+    # with col4:
+    #     st.text(names[3])
+    #     # st.image(posters[3])
+    # with col5:
+    #     st.text(names[4])
+    #     # st.image(posters[4])
+    st.text(names[0])
+    st.text(names[1])
+    st.text(names[2])
+    st.text(names[3])
+    st.text(names[4])
